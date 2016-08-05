305a472474199255a202f5d8d2e44b4e25ea98d3
Adam Cowley <adam@wecommit.co>
2016-08-05 10:54:45 +0100

    Removed sockets

--- client/client-sockets.js
+++
@@ -1,8 +0,0 @@
-const {SERVER_PORT, SERVER_HOST, SOCKET_TYPE} = require('../config');
-
-const port = process.argv[2] || 9999;
-
-
-var connection = require('socket.io')(port)
-console.log(connection);
-//connection.connect(SERVER_PORT, SERVER_HOST)
